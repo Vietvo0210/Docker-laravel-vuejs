c61a49a047890916bec935e33d9d1e9c899c2922
VietVMQ <vietvmq@smartgift.vn>
2023-07-20 15:40:04 +0700

    update docker

---
+++ V-Console/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install packages
+yarn install
+
+# yarn serve
+yarn serve --host 0.0.0.0 --port 8081
--- V-Laravel-Api/docker-entrypoint.sh
+++ V-Laravel-Api/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Migrate & seed
 php artisan migrate
-php artisan serve --host 0.0.0.0 --port 81
+php artisan serve --host 0.0.0.0 --port 82
 
 # CHMOD folder storage
 chmod -R 777 storage
