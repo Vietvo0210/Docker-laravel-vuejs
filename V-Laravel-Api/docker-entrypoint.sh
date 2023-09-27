968961229abf18cdd19d179933e3f997d34746b8
Vietvmq <vietvmq@smartgift.vn>
2023-09-27 16:43:52 +0700

    update project

--- V-Laravel-Api/docker-entrypoint.sh
+++ V-Laravel-Api/docker-entrypoint.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+# Install composer
+composer install
+
+# Update composer
+composer update
+
+# Install npm
+npm install
+
 # Migrate & seed
-php artisan migrate
-php artisan serve --host 0.0.0.0 --port 82
+if [ $APP_ENV != "production" ] && [ $HOSTNAME = "api" ]
+then
+  php artisan migrate
+  php artisan db:seed
+fi
 
 # CHMOD folder storage
 chmod -R 777 storage
 php artisan storage:link
 
+# Copy to .env
+cp .env.$APP_ENV .env
+
 # Start
 supervisord
-
-
